88ea4d3ed4f9106295ed30709b5a3cb33fee35fb
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2022-08-03 17:36:17 -0700

    install, don't ask

--- bin/bootstrap-linux.sh
+++ bin/bootstrap-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt install apt-transport-https curl
+apt install -yq apt-transport-https curl
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"| tee /etc/apt/sources.list.d/brave-browser-release.list
 apt update -yq
