dd6584c6d9183d39c63331bdc5ed9ee18d85248a
Anonymous <2119348+dzianisv@users.noreply.github.com>
2022-08-03 17:28:59 -0700

    Create boostrap-linux.sh

---
+++ bin/bootstrap-linux.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+apt install apt-transport-https curl
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"| tee /etc/apt/sources.list.d/brave-browser-release.list
+apt update
+apt install brave-browser
+
+apt install vim ffmpeg gocryptfs sshfs 
+snap install lxd vscode
