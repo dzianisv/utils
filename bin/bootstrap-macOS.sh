31ac69721eee5ae7b3068874c23ab98cfaa2097e
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2022-09-15 22:53:09 -0700

    Add boostrap-macOS.sh

---
+++ bin/bootstrap-macOS.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+brew install ffmpeg mpv
+brew install --cask brave-browser
+brew install --cask visual-studio-code
+brew install --cask libreoffice
+brew install --cask qbittorrent
+brew install --cask resilio-sync
+sudo ports install gocryptfs
+sudo ln -fsn /opt/local/Library/Filesystems/macfuse.fs /Library/Filesystems/macfuse.fs
+
