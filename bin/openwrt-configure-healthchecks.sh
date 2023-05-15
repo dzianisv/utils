7da54873118c1864dced7a02956625a25406f116
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2023-05-15 22:57:08 +0300

    Update healthchecks script

--- bin/openwrt-configure-healthchecks.sh
+++ bin/openwrt-configure-healthchecks.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-opkg update
-opkg install libustream-mbedtls
+
+set -euo pipefail
+
+if [[ -z "${UUID:-}" ]]; then
+    echo "UUID is not set"
+    exit 1
+fi
 
 # Create the daemon script
 cat << 'EOF' > /etc/init.d/healthchecks
@@ -10,42 +15,44 @@ START=99
 STOP=01
 
 USE_PROCD=1
-PROG=/usr/bin/healthchecks
+PROG=/usr/bin/healthchecks_script
 URL=$(uci get healthchecks.@healthchecks[0].url)
+INTERVAL=$(uci get healthchecks.@healthchecks[0].interval)
 
 start_service() {
     procd_open_instance
-    procd_set_param command $PROG $URL
+    procd_set_param command $PROG $URL $INTERVAL
     procd_set_param respawn
     procd_close_instance
 }
 
 stop_service() {
-    killall healthchecks
+    killall healthchecks_script
 }
 EOF
 
 chmod +x /etc/init.d/healthchecks
 
 # Create the HTTP GET script
-cat << 'EOF' > /usr/bin/healthchecks
+cat << 'EOF' > /usr/bin/healthchecks_script
 #!/bin/sh
 
 URL=$1
+INTERVAL=$2
 
 while true; do
-    wget -q -O /dev/null "\$URL"
-    sleep 3600
+    wget -q -O /dev/null "$URL"
+    sleep $INTERVAL
 done
 EOF
 
-chmod +x /usr/bin/healthchecks
+chmod +x /usr/bin/healthchecks_script
 
 # Create the UCI config
-# Replace https by http if there is not space for mbedtls
-cat << 'EOF' > /etc/config/healthchecks
+cat << "EOF" > /etc/config/healthchecks
 config healthchecks
-    option url 'https://hc-ping.com/<uuid>'
+    option url 'https://hc-ping.com/$UUID'
+    option interval '10'
 EOF
 
 # Enable and start the service
