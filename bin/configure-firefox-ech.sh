f05e5da90d7698d3614c4ca229d519d65b5a89b6
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2023-06-03 15:12:23 +0300

    update firefox configuration script

--- bin/configure-firefox-ech.sh
+++ bin/configure-firefox-ech.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# ECH also changes the key distribution and encryption stories:
+# A TLS server supporting ECH now advertises its public key via an HTTPSSVC DNS record,
+# whereas ESNI used TXT records for this purpose. Key derivation and encryption are made more robust,
+# as ECH employs the Hybrid Public Key Encryption specification rather than defining its own scheme.
+# Importantly, ECH also adds a retry mechanism to increase reliability with respect
+# to server key rotation and DNS caching. Where ESNI may currently fail after receiving stale keys from DNS,
+# ECH can securely recover, as the client receives updated keys directly from the server.
+
 # Find the Firefox profile directory
 if [[ "$(uname)" == "Darwin" ]]; then
   profile_dir=$(find ~/Library/Application\ Support/Firefox/Profiles/ -maxdepth 1 -type d -name "*.default*" -print -quit)
@@ -22,6 +30,10 @@ user_pref("network.trr.uri", "https://cloudflare-dns.com/dns-query");
 user_pref("network.trr.bootstrapMethod", 3);
 user_pref("security.tls.enable_0rtt_data", true);
 user_pref("security.tls13.echconfig.enabled", true);
+
+// https://blog.mozilla.org/security/2021/01/07/encrypted-client-hello-the-future-of-esni-in-firefox/
+user_pref("network.dns.use_https_rr_as_altsvc", true);
+user_pref("network.dns.echconfig.enabled", true); // allow Firefox to use ECH with servers that support it
 EOF
 
 echo "user.js file created in Firefox profile directory: $profile_dir"
