a94c1a811c2afb910f779b71aff2877fbd895a6e
Dennis Vashchuk <2119348+dzianisv@users.noreply.github.com>
2023-07-09 17:53:58 +0300

    Keep a modification time

--- bin/photo-apply-lut.sh
+++ bin/photo-apply-lut.sh
@@ -14,5 +14,6 @@ for i in "$@"; do
     output=${i%.*}.jpg
     # In this command, -q:v 2 sets the quality of the output JPEG image. Lower values will give higher quality. The value of 2 corresponds roughly to a 90% quality setting in many other image processing tools.
     dcraw -c -w "$i" | ffmpeg -i - -filter_complex "[0:v]split[base][lut];[lut]lut3d='${LUT:?LUT is not set}'[lutout];[base][lutout]blend=all_expr='A*(1-$LUT_INTENSITY)+B*$LUT_INTENSITY'" -q:v 2 "${output}"
+    touch -r "$i" "$output"
 done
 
