4074e91d289298a056bcbbafb378b404d7616742
engineer <engineer@engineers-MBP.lan>
2023-11-23 17:40:17 +0200

    Update healthcheck scripts

--- bin/openwrt-configure-healthchecks.sh
+++ bin/openwrt-configure-healthchecks.sh
@@ -14,22 +14,22 @@ EOF
 chmod 0755 "$SCRIPT_PATH"
 
 if [[ ! -f /etc/config/healthchecks ]]; then
-    if [[ -z "${UUID:-}" ]]; then
-        echo "UUID is not set"
+    if [[ -z "${URL:-}" ]]; then
+        echo "URL is not set"
         exit 1
     fi
 
     # Create the UCI config
     cat << "EOF" > /etc/config/healthchecks
 config healthchecks
-    option url 'https://hc-ping.com/$UUID'
+    option url '$URL'
 EOF
 fi
 
 CRON_JOB_PATH="/etc/crontabs/root"
 # Create the cron job to run script each 10m
 cat <<EOF >> "$CRON_JOB_PATH"
-*/10 * * * * $SCRIPT_PATH
+*/1 * * * * $SCRIPT_PATH
 EOF
 
 # Set permissions for the cron job file
